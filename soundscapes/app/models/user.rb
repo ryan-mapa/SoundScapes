a623a48707b310ced931df48fa36db2e8ce65213
ryan mapa <ryan.m.mapa@gmail.com>
2017-10-09 23:55:54 -0700

    Configure user model

--- soundscapes/app/models/user.rb
+++ soundscapes/app/models/user.rb
@@ -1,2 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  provider   :string
+#  uid        :string
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+
+  class User < ApplicationRecord
+  	def self.find_or_create_from_auth_hash(auth)
+  		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+  			user.provider = auth.provider
+  			user.uid = auth.uid
+  			user.name = auth.info.first_name
+  			user.email = auth.info.email
+  			user.save!
+  		end
+  	end
+  end
+
+
 end
--- soundscapes/test/models/user_test.rb
+++ soundscapes/test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  provider   :string
+#  uid        :string
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
