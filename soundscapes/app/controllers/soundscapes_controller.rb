1b5b949495b9490d48cb8d95feb6e85db4ca2aae
ryan mapa <ryan.m.mapa@gmail.com>
2017-10-12 11:14:02 -0700

    Update controller methods

--- soundscapes/app/controllers/soundscapes_controller.rb
+++ soundscapes/app/controllers/soundscapes_controller.rb
@@ -1,13 +1,18 @@
 class SoundscapesController < ApplicationController
   def create
+    @soundscape = Soundscape.new
+    @soundscape.user_id = current_user.id
+    @soundscape.save
   end
 
   def show
+    @soundscape = Soundscape.find_by_id(params[:soundscape_id])
   end
 
   def destroy
   end
 
   def index
+    @soundscapes = Soundscape.all
   end
 end
