fc60dceb641659cc2e68f0ac25d5bb2fe90072fc
ryan mapa <ryan.m.mapa@gmail.com>
2017-10-11 19:58:17 -0700

    Test commit

---
+++ soundscapes/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to :me
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end
---
+++ soundscapes/config/initializers/omniauth.rb
@@ -0,0 +1,15 @@
+keys = Rails.application.secrets
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify, '2d9b5153c47946eea1f7af099022b036', '59bb6f06609348cb841ce36a728f7594', scope: 'playlist-read-private user-read-private user-read-email'
+end
+
+# flash[:ommiauth_spotify_force_approval?]
+# replaced values:
+# keys.spotify['client_id']
+# keys.spotify['client_secret']
+
+# Rails.application.secrets.oauth_key
+# Rails.application.secrets.oauth_secret
+
+# :facebook, '175674496323311', 'b3501e76c5531afbe8ef266aca8974cf'
