69f89fe99a73674945448631f6452db50d97b2d3
= <pjfritch1@gmail.com>
2017-10-15 14:19:09 -0700

    create store to connect react and redux

--- soundscapes/app/frontend/SoundScapesFrontend/index.js
+++ soundscapes/app/frontend/SoundScapesFrontend/index.js
@@ -2,6 +2,16 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import configureStore from './store/store';
 
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { Provider } from 'react-redux';
+
 const store = configureStore();
 
 const Root = () => (
